ca4ea7ee68d3a68723466ee5ca317e4b829ed0de
Ike Yang <70048185+zhinendemofasshi@users.noreply.github.com>
2023-04-21 00:49:01 +0800

    connected to firebase

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -2,11 +2,17 @@
 "use client";
 
 import NewChat from "@/components/NewChat";
+import { useCollection } from "react-firebase-hooks/firestore";
 import { useSession, signOut } from "next-auth/react";
+import { collection } from "firebase/firestore";
+import db from "@/firebase";
 
 function SideBar() {
   const { data: session } = useSession();
-
+  const [chats, loading, error] = useCollection(
+    session && collection(db, "users", session.user?.email!, "chats")
+  );
+  console.log(chats)
   return (
     <div className="p-4 flex flex-col h-screen bg-green-200">
       <div className="flex-1">
@@ -14,7 +20,11 @@ function SideBar() {
           {/*new chat */}
           <NewChat />
           <div>{/*selection*/}</div>
+          {/* start from here */}
           {/*something more like chatrows */}
+          {/* {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))} */}
         </div>
       </div>
 
