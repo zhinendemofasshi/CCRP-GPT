a436553210b92400624eae96b592ef44912295e0
Ike Yang <70048185+zhinendemofasshi@users.noreply.github.com>
2023-04-27 13:40:37 +0800

    restructure source code

--- lib/queryApi.ts
+++ lib/queryApi.ts
@@ -1,19 +1,22 @@
 import openai from "./chatgpt";
 
 const query = async (prompt: string, chatId: string, model: string) => {
-    const res = await openai.createCompletion({
-        model, 
-        prompt,
-        temperature: 0.9,
-        top_p: 1,
-        max_tokens: 1000,
-        frequency_penalty: 0,
-        presence_penalty: 0,
+  const res = await openai
+    .createCompletion({
+      model,
+      prompt,
+      temperature: 0.9,
+      top_p: 1,
+      max_tokens: 1000,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     })
-    .then(res => res.data.choices[0].text)
-    .catch(err => `GPT could not find a answer for that! (Error: ${err.message})`)
+    .then((res) => res.data.choices[0].text)
+    .catch(
+      (err) => `GPT could not find a answer for that! (Error: ${err.message})`
+    );
 
-    return res;
-}
+  return res;
+};
 
 export default query;
