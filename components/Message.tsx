6cd88ffb835ff2e4eff36847f3939531a3414ce0
mengru-wang <mengru-wang@outlook.com>
2023-05-12 20:59:06 +0800

    Try changing styles

--- components/Message.tsx
+++ components/Message.tsx
@@ -8,7 +8,7 @@ function Message({ message }: Props) {
   const isChatGPT = message.user._id === "chatGPT";
   //   console.log(message.text);
   return (
-    <div className={`py-5 text-white ${isChatGPT && "bg-violet-400"}`}>
+    <div className={`py-5 text-white chatmessage ${isChatGPT && "bg-violet-400"}`}>
       <div className="flex space-x-5 px-10 max-w-2xl mx-auto">
         <img src={message.user.avatar} alt="" className="h-8 w-8" />
         <p className="pt-2 text-sm"> {message.text} </p>
